a8e494c89963b73361bb1d2ba61c63d045cebb21
sabzj <zjsabbah@gmail.com>
2023-10-02 18:54:13 +0300

    trial3

---
+++ 2.1.funk.js
@@ -0,0 +1,7 @@
+function countryInfo(country, population, capitalcity){
+  // const name = "zafer"
+  return `${country} has ${population} people ant its ${capitalcity}`
+}
+console.log(countryInfo)
+
+countryInfo ("spain", "47 Million","Madrid");
---
+++ 2.3.funk.js
@@ -0,0 +1,8 @@
+function calcCircle(cirradius){
+   const result= Math.PI*Math.pow(cirradius, 2) 
+   console.log(result)
+   console.log(result.toFixed(2));
+   
+}
+
+calcCircle(30)
